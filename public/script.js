cde250a579e2569d9f753c6317edd62ffa99792d
Hoang Dinh <hoangdinh90@gmail.com>
2020-08-03 16:35:42 -0700

    creating user connection

--- public/script.js
+++ public/script.js
@@ -14,13 +14,14 @@ navigator.mediaDevices
   })
   .then((stream) => {
     addVideoStream(myVideo, stream);
+
+    socket.on("user-connected", (userId) => {
+      connectToNewUser(userId, stream);
+    });
   });
+
 myPeer.on("open", (id) => {
   socket.emit("join-room", ROOM_ID, id);
-});
-
-socket.on("user-connected", (userId) => {
-  console.log("User connceted", userId);
 });
 
 function addVideoStream(video, stream) {
@@ -29,4 +30,16 @@ function addVideoStream(video, stream) {
     video.play();
   });
   videoGrid.append(video);
+}
+
+function connectToNewUser(userId, stream) {
+  const call = myPeer.call(userId, stream);
+  const video = document.createElement("video");
+
+  call.on("stream", (userVideoStream) => {
+    addVideoStream(video, userVideoStream);
+  });
+  call.on("close", () => {
+    video.remove();
+  });
 }
